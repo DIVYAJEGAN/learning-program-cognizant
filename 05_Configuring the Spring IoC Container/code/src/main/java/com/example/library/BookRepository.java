c587a597a14f0a38f9785a288044a1b01127bc77
DIVYA J <124855134+DIVYAJEGAN@users.noreply.github.com>
2025-07-07 08:58:39 +0530

    Delete 05_Configuring the Spring IoC Container directory

--- 05_Configuring the Spring IoC Container/code/src/main/java/com/example/library/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        System.out.println("Book '" + bookName + "' saved to repository.");
-    }
-}
