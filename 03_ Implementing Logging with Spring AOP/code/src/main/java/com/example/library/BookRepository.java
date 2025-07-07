224790d95d9294762f62d1b2427e4217167f9ff9
DIVYA J <124855134+DIVYAJEGAN@users.noreply.github.com>
2025-07-07 08:58:18 +0530

    Delete 03_ Implementing Logging with Spring AOP directory

--- 03_ Implementing Logging with Spring AOP/code/src/main/java/com/example/library/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        System.out.println("BookRepository: Saving book - " + bookName);
-    }
-}
