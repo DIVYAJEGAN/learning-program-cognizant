a122271d3bf7dede5a47f77e403a8c192dea54bf
DIVYA J <124855134+DIVYAJEGAN@users.noreply.github.com>
2025-07-07 08:59:01 +0530

    Delete 07_Implementing Constructor and Setter Injection directory

--- 07_Implementing Constructor and Setter Injection/code/src/main/java/com/library/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library;
-
-public class BookRepository {
-    public void saveBook(String bookName) {
-        System.out.println("Book '" + bookName + "' saved to repository.");
-    }
-}
